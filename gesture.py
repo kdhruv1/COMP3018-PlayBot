10521bf4753d4026071e0980b52738bfd179f6ce
cjefferies19 <connor.jefferies@students.plymouth.ac.uk>
2025-05-02 16:19:08 +0100

    Update gesture.py

--- gesture.py
+++ gesture.py
@@ -1,49 +1,22 @@
-import rospy
-import sys
-import tf2_ros
-import tf2_geometry_msgs  # for do_transform_pose
-from geometry_msgs.msg import PoseStamped
-from moveit_commander import MoveGroupCommander, roscpp_initialize
+# pip install opencv-python pyzbar pillow
+# python gesture.py
+from camera_win import subscribe
 
-class GestureNode:
-    def __init__(self):
-        roscpp_initialize(sys.argv)
-        rospy.init_node('gesture_node')
+def gesture_to_card(data):
+    text = data['text']
+    pose = data['pose']  # (x_pix, y_pix)
+    print(f">>> [Gesture] Pointing at '{text}' located at pixel {pose}")
 
-        # MoveIt setup
-        self.arm = MoveGroupCommander("manipulator")
-        self.arm.set_planning_time(5.0)
-
-        # TF for transforming camera_link to base_link
-        self.tf_buf  = tf2_ros.Buffer()
-        self.tf_lisn = tf2_ros.TransformListener(self.tf_buf)
-
-        rospy.Subscriber('/detected_card_pose', PoseStamped, self.card_cb, queue_size=1)
-        rospy.loginfo("Gesture node ready.")
-        rospy.spin()
-
-    def card_cb(self, ps_msg):
-        try:
-            # transform into base_link
-            target = self.tf_buf.transform(ps_msg, 'base_link', rospy.Duration(1.0))
-            pose_goal = PoseStamped()
-            pose_goal.header.frame_id = 'base_link'
-            pose_goal.pose = target.pose
-
-            rospy.loginfo("Pointing to card at (%.3f, %.3f, %.3f)",
-                          pose_goal.pose.position.x,
-                          pose_goal.pose.position.y,
-                          pose_goal.pose.position.z)
-
-            self.arm.set_pose_target(pose_goal)
-            self.arm.go(wait=True)
-            self.arm.stop()
-            self.arm.clear_pose_targets()
-        except Exception as e:
-            rospy.logerr("Gesture error: %s", e)
+def main():
+    subscribe('card', gesture_to_card)
+    print("Waiting for QR scans to gesture at...")
+    try:
+        # Keep alive
+        import time
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nGesture node stopped.")
 
 if __name__ == '__main__':
-    try:
-        GestureNode()
-    except rospy.ROSInterruptException:
-        pass
+    main()
